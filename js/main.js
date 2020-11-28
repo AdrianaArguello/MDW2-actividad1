b6a6efc68de33a1835be1bdb939e9cb752e2fd7c
marguello <mariana.arguello@urbe.edu>
2020-11-28 17:22:16 -0400

    initial

---
+++ js/main.js
@@ -0,0 +1,5 @@
+ScrollReveal().reveal('.page-section1');
+ScrollReveal().reveal('.page-section2');
+ScrollReveal().reveal('.page-section3');
+ScrollReveal().reveal('.page-section4');
+ScrollReveal().reveal('.page-section-carousel');
